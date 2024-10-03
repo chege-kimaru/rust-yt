e8d51bde9580b19350f13fe6b87287fa8d2b79ea
chege-kimaru <chege.kimaru@gmail.com>
2024-10-03 19:52:51 +0300

    api requests

---
+++ async_rust/src/main.rs
@@ -0,0 +1,18 @@
+use error_chain::error_chain;
+
+error_chain! {
+    foreign_links {
+        Io(std::io::Error);
+        Httprequest(reqwest::Error);
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<()>{
+    let res = reqwest::get("http://httpbin.org/get").await?;
+    println!("Status: {}", res.status());
+    println!("Headers: \n{:#?}", res.headers());
+    let body = res.text().await?;
+    println!("Body:\n {}", body);
+    Ok(())
+}
